c29bf28eb20b2bd7c5eff928a0a3c3becd8f1e40
SavigeRL <rdjon5@schools.vic.edu.au>
2026-05-28 09:18:55 +1000

    Correct event listener spelling for theme toggle
    Fix typo in event listener for theme toggle.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ if (savedTheme) {
 
 // Add click event listener to toggle button
 themeToggle.addEventListener('click', () => {
+  themeToggle.addEventListner('d',()=> {
   const currentTheme = body.classList.contains('dark-mode') ? 'dark' : 'light';
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
   
